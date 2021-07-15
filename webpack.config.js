13a331cdfaca079e600a51daebff25340b3188be
CBear <chebert222@gmail.com>
2021-07-14 20:03:20 -0700

    split script file into a lazy loading structure

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,39 @@
 const path = require('path');
 const webpack = require('webpack');
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
 
 module.exports = {
-    entry: './assets/js/script.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'main.bundle.js'
-    },
-    mode: 'development',
-    plugins:[
-        new webpack.ProvidePlugin({
-          $: "jquery",
-          jQuery: "jquery"
-        }),
-      ],
+  entry: {
+    app: './assets/js/script.js',
+    events: './assets/js/events.js',
+    schedule: './assets/js/schedule.js',
+    tickets: './assets/js/tickets.js'
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.join(__dirname, '/dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jpg$/i,
+        use: [
+          {
+            loader: 'file-loader'
+          }
+        ]
+      }
+    ]
+  },
+  mode: 'development',
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery"
+    }),
+    new BundleAnalyzerPlugin({
+      analyzerMode: 'static', // the report outputs to an HTML file in the dist folder
+    })
+  ],
 };
